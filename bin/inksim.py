c287336586f2dfc39d0d81830df97ae184f5205d
karnigen <karnigen@gmail.com>
2026-08-05 00:12:09 +0200

    Add venv auto-activation and wx import [no ci]

--- bin/inksim.py
+++ bin/inksim.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 import sys
+import os
+
+script_dir = Path(__file__).resolve().parent
+venv_dir = (script_dir / ".." / ".venv").resolve()
+
+def ensure_venv():
+    # print(f"{sys.prefix=}"); print(f"{sys.base_prefix=}")
+    if sys.prefix != sys.base_prefix: # we are in a virtual environment
+        return
+
+    # Path to the Python .venv (Linux/macOS vs Windows)
+    if os.name == "nt":
+        venv_python = venv_dir / "Scripts" / "python.exe"
+    else:
+        venv_python = venv_dir / "bin" / "python"
+
+    if venv_python.exists():
+        os.execv(venv_python, [venv_python] + sys.argv)
+    else:
+        print(f"Warning: Virtual environment not found at {venv_dir}. Running with system Python.", file=sys.stderr)
+
+# start the virtual environment if not already active
+ensure_venv()
+
+import wx
 
 def main():
     print("PES Viewer")
